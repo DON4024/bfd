88a0974589a158f0f67735030bf3b2c016c5f5ea
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-03 11:43:46 +0900

    create main-view-usr-icon, comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+  def create
+    Comment.create(comment_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, picture_id: params[:picture_id])
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200301021717) do
+ActiveRecord::Schema.define(version: 20200302102424) do
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "comments"
+    t.integer  "user_id"
+    t.integer  "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_comments_on_picture_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "pictures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "image",      null: false
@@ -36,5 +46,7 @@ ActiveRecord::Schema.define(version: 20200301021717) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "pictures"
+  add_foreign_key "comments", "users"
   add_foreign_key "pictures", "users"
 end
