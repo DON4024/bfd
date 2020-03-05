2400e312ac3afa2302805e94081bec82b5f062fc
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-05 19:44:37 +0900

    fix favorite-destroy

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,32 +5,25 @@ class FavoritesController < ApplicationController
       user_id = current_user.id
       picture_id = Picture.find(params[:picture_id]).id
       judg = params[:judg_id]
-      
-      @temporary = current_user.judgment(judg: judg, user_id: user_id, picture_id: picture_id)
-      # @temporary = Cute.new(picture_id: @picture_id, user_id: @user_id)
+      @temporary = current_user.judgment_new(judg: judg, user_id: user_id, picture_id: picture_id)
       @temporary.save
 
       redirect_to root_path
     end
   
     def destroy
-      @temporary = Favorite.find_by(picture_id: params[:id])
-      case(judg)
-        when 1
-          @cool = @temporary
-          @cool.destroy
-        when 2
-          @cute = @temporary
-          @cute.destroy
-        when 3
-          @creepy = @temporary
-          @creepy.destroy
-        when 4
-          @favorite = @temporary
-          @favorite.destroy
-      end
+      user_id = current_user.id
+      picture_id = Picture.find(params[:id]).id
+      judg = params[:judg_id]
+      @temporary = current_user.judgment_destroy(judg: judg, user_id: user_id, picture_id: picture_id)
+      @temporary.destroy
+
+      redirect_to root_path
+
+
+
+
       
-        redirect_to root_path
       
     end
 
