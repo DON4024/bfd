7a179b86453056b7a5970f4498601d69909a0ddd
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-11 13:00:41 +0900

    create main-view-animate

--- app/assets/javascripts/main-view.js
+++ app/assets/javascripts/main-view.js
@@ -1,16 +1,27 @@
 $(function(){
-  $('.review-items__icons__open').on('click', function(){
-    
-  
-    var selectgroup = $('#app-main__body__group').attr('class')
-    console.log(selectgroup)
-    if($('.review-items__icons__open').hasClass('off')){
-      $('.review-items__icons__open').removeClass('off');
-      $(`.${selectgroup}`).animate({'paddingLeft':'0px'},300);
-    }else{
-      $('.review-items__icons__open').addClass('off');
-      $(`.${selectgroup}`).animate({'paddingLeft':'500px'},300);
+  $('.review-items__icons').on('click', function(){
+    console.log(this)
+    $(this).removeAttr("disabled");
+    var selectopen = $(this).attr('class');
+    selectopen = selectopen.replace( ' review-items__icons', '')
+    console.log(selectopen)
+    if($(this).css("margin-left").length==5){
+      console.log($(this).css("margin-left").length)
+      $(`.${selectopen}`).animate({'marginLeft':'0'},300);
+    }else {
+      $(`.${selectopen}`).animate({'marginLeft':'310px'},300);
     }
+    // var selectgroup = $('#app-main__body__group').attr('class');
+    // console.log(selectgroup)
+    
+    // if($(`${selectopen}`).hasClass('off')){
+    //   $(`${selectopen}`).removeClass('off');
+    //   $(`.${selectgroup}`).animate({'paddingLeft':'0px'},300);
+    //   selectgroup = ""
+    // }else{
+    //   $('#review-items__icons__open').addClass('off');
+    //   $(`.${selectgroup}`).animate({'paddingLeft':'550px'},300);
+    // }
 
   })
 })
