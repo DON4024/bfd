d2d048fcc6feef9129230d6acba3964b2538388e
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-14 19:01:53 +0900

    carrierwave production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,23 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
-  }
+if Rails.env.development? 
+  
 
-  config.fog_directory  = 'bfd-image-store'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bfd-image-store'
+
+elsif Rails.env.production? 
+
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
+    }
+
+    config.fog_directory  = 'bfd-image-store'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bfd-image-store'
+  end
 end
