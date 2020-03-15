337c5fc8b71f886a8ac957ace517a489a3e064ed
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-15 11:44:22 +0900

    fix jsfile

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -1,76 +1,63 @@
 $(function(){
-  // function buildselect(){
-  //   var htmlselect =`
-  //         <form action="/" accept-charset="UTF-8" method="post">
-  //           <input name="utf8" type="hidden" value="✓">
-  //           <input type="hidden" name="authenticity_token" value="soYlvBK2rBkOAEOU9et6kUNTLWbV/q7uC7TqSWftV3aot7GzF/7U9d9x7DYVGdappS2lUWxcpy5RbWKbU4zfRA==">
-  //           <select class="sort-box" name="javascript:void()[image]" id="javascript:void__image">
-  //             <option value="0">all</option>
-  //             <option value="1">cool</option>
-  //             <option value="2">cute</option>
-  //             <option value="3">creepy</option>
-  //           </select>
-  //         </form>`
-
-  //   return htmlselect
-  // }
   function buildpost(data){
     var html = `
-          <div class="app-main__body__group">
-            <div class="picture-image" data-post-id=${data.id}>
-              <div class="group-id88 move-flame" >
-                <i class="fas fa-expand fa-2x"></i>
-                <div class="group-id${data.id} review-items__icons">
-                </div>
-                <a class="image-favorite" rel="nofollow" data-method="post" href="javascript:void(76, 4)">
-                  <img class="bests-pic image-url" src=${data.image} alt="%e6%b5%b7%e8%b3%8a">
-                  <input name="[0][picture_id:]" type="hidden">
-                </a>
-              </div>
-              <div class="review-items__icons__list">
-                <div class="review-items__icons__list__left">
-                  <a rel="nofollow" data-method="post" href="/pictures/76/add?judg_id=1">
-                    <i class="fas fa-grin-stars fa-3x"></i>
-                  </a>
-                </div>
-                <div class="review-items__icons__list__center">
-                  <a rel="nofollow" data-method="post" href="/pictures/76/add?judg_id=2">
-                    <i class="far fa-grin-hearts fa-3x"></i>
-                  </a>
-                </div>
-                <div class="review-items__icons__list__right">
-                  <a rel="nofollow" data-method="post" href="/pictures/76/add?judg_id=3">
-                    <i class="far fa-flushed fa-3x"></i>
-                  </a>
-                </div>
-              </div>
-                
-
-
-              <div class="review-group">
-                <div class="review-group__items">
-                  <div class="picture-name">
-                    ${data.nickname}
+    <div class="app-main__body__group">
+    <div class="picture-image" data-post-id=${data.id}>
+      <div class="move-flame">
+        <a class="image-favorite" rel="nofollow" data-method="delete" href="javascript:void(92, 4)">
+          <img class="bests-pic image-url" src=${data.image} alt="Sanji">
+        </a>
+      </div>
+      <div class="review-items__icons__list">
+        <div class="review-items__icons__list__left">
+          <a rel="nofollow" data-method="post" href="/pictures/92/add?judg_id=1">
+            <i class="fas fa-grin-stars fa-3x"></i>
+          </a>
+        </div>
+        <div class="review-items__icons__list__center">
+          <a rel="nofollow" data-method="post" href="/pictures/92/add?judg_id=2">
+            <i class="far fa-grin-hearts fa-3x"></i>
+          </a>
+        </div>
+        <div class="review-items__icons__list__right">
+          <a rel="nofollow" data-method="post" href="/pictures/92/add?judg_id=3">
+            <i class="far fa-flushed fa-3x"></i>
+          </a>
+        </div>
+        <a class="move-btn" href="javascriot:void(group-id${data.id})">
+          <i class="fas fa-expand fa-2x"></i>
+        </a>
+      </div>
+      <div class="group-id92 review-group">
+        <div class="review-group__items">
+          <div class="picture-name">
+            <a class="btn" href="/users/13">edward
+            </a>
+          </div>
+          <div class="review-items">
+            <div class="review-items__content">
+              ${data.text}
+            </div>
+            <div class="review-items__comments">
+              <div class="comments-box">
+                <form class="new_comment" id="new_comment" action="/pictures/92/comments" accept-charset="UTF-8" method="post">
+                  <input name="utf8" type="hidden" value="✓">
+                  <input type="hidden" name="authenticity_token" value="1VfGJNuevIl1GZqwVpZV+Fjuls4pstg2qkfYiFVU1EgXf/ITHoYC0hgZvpeKVZS6t4zLxZzDRPJMWqPMePbCeA==">
+                  <div class="input-box">
+                    <input class="message-box" placeholder="コメントをどうぞ" type="text" name="comment[comments]" id="comment_comments">
+                    <input type="submit" name="commit" value="Send" class="submit-btn" data-disable-with="Send">
                   </div>
-                  <div class="review-items">
-                    <div class="review-items__content">
-                      ${data.content}
-                    </div>
-                    <div class="review-items__comments">
-                      <div class="comments-box">
-                        <div class="input-box">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                </form>
               </div>
-            
-          </div>`
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`
     return html;
   }
   $('.sort-box').on('change', function(){
-    console.log("ok");
     var select = $('option:selected').val();
     $.ajax({
       type: 'GET',
@@ -80,8 +67,6 @@ $(function(){
     })
     .done(function(pictures){
       $( '#post-body' ).empty();
-      // var htmlselect = buildselect();
-      // $('#post-body').append( htmlselect );
       pictures.forEach( function(picture) {
         var html = buildpost(picture);
         $('#post-body').append( html );
